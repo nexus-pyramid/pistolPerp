311748d5811649266ae98cd8524401e822a5a5d4
Mac <mac@MacBook-Pro.local>
2018-10-17 13:57:00 -0400

    mobile view and font changes

--- public/assets/app.js
+++ public/assets/app.js
@@ -6,6 +6,14 @@ app.config( function ($routeProvider, $locationProvider, $httpProvider) {
     templateUrl:   '/assets/partials/main.html',
     controller: 'smokeController'
    })
+   .when('/listen', {
+    templateUrl:   '/assets/partials/tracks.html',
+    controller: 'smokeController'
+   })
+    .when('/', {
+    templateUrl:   '/assets/partials/main.html',
+    controller: 'smokeController'
+   })
   .otherwise({
     redirectTo: '/'
   });
